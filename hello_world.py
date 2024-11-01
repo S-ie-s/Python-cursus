48f57565314adf02ee342958844324dafbd34041
Sies Frauenfelder <siesfrauenfelder@gmail.com>
2024-11-01 12:08:12 +0100

    First hello world print

---
+++ hello_world.py
@@ -0,0 +1,17 @@
+from datetime import date
+
+# this is an comment 
+
+message = 'Good morning'
+today = date.today()
+
+print('hello world!')
+
+print(message)
+
+#message = 'hello world!'
+
+#print(message)
+print( today)
+
+
