7bfe7b054c7437d8147aac0c93d0df99b4745809
Sies Frauenfelder <siesfrauenfelder@gmail.com>
2024-11-11 11:44:11 +0100

    created exercise 16

--- Student Files/iterables/Exercises/remove_random.py
+++ Student Files/iterables/Exercises/remove_random.py
@@ -2,9 +2,16 @@ import random
 
 def remove_random(the_list):
     pass # replace this with your code
+    print(the_list)
+    random_color = random.choice(the_list)
+    print(random_color)
+    the_list.remove(random_color)
+    print(the_list)
 
 def main():
     colors = ['red', 'blue', 'green', 'orange']
+    remove_random(colors)
+    
     # Your code here
 
 main()
