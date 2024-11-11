ea3c6f7f8be6a7faf6f09d1d0b01608628f99511
Sies Frauenfelder <siesfrauenfelder@gmail.com>
2024-11-11 12:18:49 +0100

    rock paper scissors game created

--- Student Files/iterables/Exercises/roshambo.py
+++ Student Files/iterables/Exercises/roshambo.py
@@ -2,5 +2,18 @@ import random
 
 def main():
     pass # replace this with your code
+    rps_seq = ['Rock', 'Paper', 'Scissors']
+    random_rps = random.choice(rps_seq)
+    user_choice = int(input("1 for rock, 2 for paper, 3 for scissors "))
+
+    user_rps = rps_seq[user_choice - 1]
+    
+    
+    print("computer: ", random_rps)
+    print("user: ", user_rps)
+
+    
+    
+    
 
 main()
