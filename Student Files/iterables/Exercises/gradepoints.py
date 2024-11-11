fa62f2df822b1af0a751c5394306de5d1fb64044
Sies Frauenfelder <siesfrauenfelder@gmail.com>
2024-11-11 15:40:34 +0100

    created grades exercise

--- Student Files/iterables/Exercises/gradepoints.py
+++ Student Files/iterables/Exercises/gradepoints.py
@@ -1,4 +1,35 @@
-def main():
-    pass # replace this with your code
+import math
 
+def main():
+    math_grade = int(input("Math grade: "))
+    english_grade = int(input("English grade: "))
+    global_st_grade = int(input("Gloabal Studies grade: "))
+    art_grade = int(input("Art grade: "))
+
+    grades = {"English" : english_grade,
+              "Math" : math_grade,
+              "Global Studies" : global_st_grade,
+              "Art" : art_grade}
+    
+    
+    average_grade = sum(grades.values()) / len(grades)
+
+    print(grades)
+    print("Your average grade is a ", average_grade)
+
+    changed_subject = input("Change grade for which subject? ")
+    new_grade = int(input("Grade for " + changed_subject + " ?" ))
+    grades[changed_subject] = new_grade
+
+    average_grade = sum(grades.values()) / len(grades)
+
+    print(grades)
+    print("Your average grade is a ", average_grade)
+
+
+
+
+
+
+ 
 main()
